721a7dd934898785b3e0d00dbb1a4a55903d5dc5
jgoralcz <jgoralcz@asu.edu>
2019-12-27 20:09:22 -0700

    forgot semicolon lol

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ server.use('/', router, errorHandler);
 
 if (env.toUpperCase() === PROD || env.toUpperCase() === TEST) {
   const certificate = { key: fs.readFileSync(serverKey), cert: fs.readFileSync(serverCert) };
-  https.createServer(certificate, server).listen(port, () => logger.info(`${env.toUpperCase()} https server started on ${port}.`))
+  https.createServer(certificate, server).listen(port, () => logger.info(`${env.toUpperCase()} https server started on ${port}.`));
 } else {
   server.listen(port, () => logger.info(`${env.toUpperCase()} server started on ${port}.`));
 }
