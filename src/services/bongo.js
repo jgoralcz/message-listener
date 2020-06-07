a27346389fd5a1c3f83904308beec87ea10db169
jgoralcz <35234547+jgoralcz@users.noreply.github.com>
2020-06-06 22:45:34 -0700

    merge (#4)
    * k8s support and 24h
    
    * fixes
    
    * update to configmap based on environment
    
    * fix to conflicts; forgot to save file
    
    * fix to api

--- src/services/bongo.js
+++ src/services/bongo.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const { basicAuth, config } = require('../util/constants/paths');
 
 // eslint-disable-next-line import/no-dynamic-require
-const { api } = require(config);
+const { bongoBotAPI: api } = require(config);
 // eslint-disable-next-line import/no-dynamic-require
 const { username, password } = require(basicAuth);
 
