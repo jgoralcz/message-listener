7aa1f706a55c789d1a4787409b702c53d8913bbf
Joshua Goralczyk <joshgor01@gmail.com>
2021-05-28 00:03:56 -0700

    shows if patron

--- src/routes/votes.js
+++ src/routes/votes.js
@@ -8,18 +8,22 @@ const { voteChannel } = require(config);
 
 route.post('/', async (req, res) => {
   const channel = client.channels.get(voteChannel);
-  const { userID, streak, points } = req.body;
+  const {
+    userID,
+    streak,
+    points,
+    isPatron,
+  } = req.body;
 
   if (!userID || !streak || !points) return res.status(400).send({ error: 'Invalid Input', message: 'Body missing userID, streak, and points' });
 
   const user = await client.fetchUser(userID);
 
-  await channel.send(`${user.tag} has received ${points} points, stored +1 roll reset, and is on a ${streak} day voting streak.`, {
+  await channel.send(`${user.tag} ${isPatron ? '(is a patron) ' : ''}has received ${points} points, stored +1 roll reset, and is on a ${streak} day voting streak.`, {
     split: true,
     code: 'js',
   });
   return res.status(200).send();
 });
-
 
 module.exports = route;
